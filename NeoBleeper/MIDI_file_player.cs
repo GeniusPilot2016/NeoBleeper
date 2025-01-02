7b3d012bdacc4ed9c74b72a28d05a134f8c72232
Nisa Özdoğan <nisaozdogan280@gmail.com>
2025-01-02 22:48:48 +0300

    MIDI player is adding

---
+++ NeoBleeper/MIDI_file_player.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NeoBleeper
+{
+    public partial class MIDI_file_player : Form
+    {
+        public MIDI_file_player()
+        {
+            InitializeComponent();
+        }
+
+        private void progressBar1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
