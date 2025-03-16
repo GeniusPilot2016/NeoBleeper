e1a345babf29b37a81f66cb2a4f723a44c94f984
Nisa Özdoğan <222303026@ogr.uludag.edu.tr>
2025-03-16 19:53:59 +0300

    Revert "Error handling for MIDI files has been improved and the MIDI player of NeoBleeper has started to become functional."
    This reverts commit 7e0846ef755a0a16122bc9eb82f944d190568b7f.

--- NeoBleeper/MIDI_file_player.cs
+++ NeoBleeper/MIDI_file_player.cs
@@ -1,5 +1,4 @@
-﻿using NAudio.Midi;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -14,7 +13,6 @@ namespace NeoBleeper
 {
     public partial class MIDI_file_player : Form
     {
-        bool is_playing = false;
         PrivateFontCollection fonts = new PrivateFontCollection();
         public MIDI_file_player(string filename)
         {
@@ -180,31 +178,24 @@ namespace NeoBleeper
         }
         private bool IsMidiFile(string filePath)
         {
-            try
-            {
-                if (!File.Exists(filePath))
-                {
-                    return false;
-                }
-
-                byte[] header = new byte[4];
-                using (FileStream fs = new FileStream(filePath, FileMode.Open, FileAccess.Read))
-                {
-                    fs.Read(header, 0, 4);
-                }
-
-                // MIDI files always start with the header MThd
-                return header[0] == 'M' && header[1] == 'T' && header[2] == 'h' && header[3] == 'd';
-            }
-            catch (Exception)
+            if (!File.Exists(filePath))
             {
                 return false;
             }
+
+            byte[] header = new byte[4];
+            using (FileStream fs = new FileStream(filePath, FileMode.Open, FileAccess.Read))
+            {
+                fs.Read(header, 0, 4);
+            }
+
+            // MIDI files always start with the header MThd
+            return header[0] == 'M' && header[1] == 'T' && header[2] == 'h' && header[3] == 'd';
         }
 
         private void button4_Click(object sender, EventArgs e)
         {
-            openFileDialog.Filter = "MIDI Files|*.mid";
+            openFileDialog.Filter = "MIDI Files|*.mid|All Files|*.*";
             openFileDialog.ShowDialog(this);
             if (openFileDialog.FileName != string.Empty)
             {
@@ -235,279 +226,18 @@ namespace NeoBleeper
         {
             if (e.Data.GetDataPresent(DataFormats.FileDrop))
             {
-                try
+                string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+                string fileName = files[0];
+                string first_line = File.ReadLines(fileName).First();
+                if (IsMidiFile(fileName))
                 {
-                    string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
-                    string fileName = files[0];
-                    string first_line = File.ReadLines(fileName).First();
-                    if (IsMidiFile(fileName))
-                    {
-                        textBox1.Text = fileName;
-                    }
-                    else
-                    {
-                        MessageBox.Show("The file you dragged is not supported by NeoBleeper MIDI player or is corrupted.", String.Empty, MessageBoxButtons.OK, MessageBoxIcon.Error);
-                    }
-                }
-                catch (Exception)
-                {
-                    MessageBox.Show("The file you dragged is corrupted or the file is in use by another process.", String.Empty, MessageBoxButtons.OK, MessageBoxIcon.Error);
-                }
-            }
-        }
-        // Static variables
-        private static bool isPlaying = false;
-        private static CancellationTokenSource cancellationTokenSource;
-        private static long totalMidiDuration; // Store total MIDI duration
-        private static Label staticLabelPosition; // Static references
-        private static Label staticLabelPercentage;
-
-        // Play method
-        public void play_MIDI_file(string filename)
-        {
-            // Assign labels into static references
-            staticLabelPosition = label_position;
-            staticLabelPercentage = label_percentage;
-
-            // Stop current playback if playing
-            StopPlayback();
-
-            // Create new CancellationTokenSource
-            cancellationTokenSource = new CancellationTokenSource();
-            var token = cancellationTokenSource.Token;
-            isPlaying = true;
-
-            // Play in seperate thread
-            Task.Run(() =>
-            {
-                try
-                {
-                    PlayMidiFileInternal(filename, token);
-                }
-                catch (OperationCanceledException)
-                {
-                    // Çalma iptal edildi
-                    UpdatePositionLabels(0, 0); // Sıfırla
-                }
-                finally
-                {
-                    isPlaying = false;
-                }
-            }, token);
-        }
-
-        // Method of label updating
-        private static void UpdatePositionLabels(long currentPosition, long totalDuration)
-        {
-            if (staticLabelPosition != null && staticLabelPercentage != null)
-            {
-                // Run in UI thread
-                if (staticLabelPosition.InvokeRequired)
-                {
-                    staticLabelPosition.Invoke(new Action(() => UpdatePositionLabels(currentPosition, totalDuration)));
-                    return;
-                }
-
-                    // Calculate position (minute:second:decimal)
-                    TimeSpan position = TimeSpan.FromMilliseconds(currentPosition);
-                staticLabelPosition.Text = string.Format("Position: {0:00}:{1:00}.{2:00}",
-                    position.Minutes, position.Seconds, Math.Truncate(Convert.ToDouble(position.Milliseconds / 100)));
-
-                // Calculate percentage
-                double percentage = (totalDuration > 0) ? (currentPosition * 100.0 / totalDuration) : 0;
-                staticLabelPercentage.Text = string.Format("%{0:00.00}", percentage);
-            }
-        }
-
-        private static void PlayMidiFileInternal(string filename, CancellationToken token)
-        {
-            var midiFile = new MidiFile(filename, false);
-            var noteEvents = new List<NoteEvent>();
-
-            // Calculate total duration
-            totalMidiDuration = CalculateTotalMidiDuration(midiFile);
-
-            foreach (var track in midiFile.Events)
-            {
-                foreach (var midiEvent in track)
-                {
-                    if (midiEvent.CommandCode == MidiCommandCode.NoteOn)
-                    {
-                        var noteEvent = (NoteEvent)midiEvent;
-                        noteEvents.Add(noteEvent);
-                    }
-                }
-            }
-
-            // Group and list notes
-            var groupedNotes = noteEvents.GroupBy(e => e.AbsoluteTime)
-                                         .OrderBy(g => g.Key)
-                                         .ToList();
-
-            // if there's no note, return
-            if (groupedNotes.Count == 0)
-                return;
-
-            long previousTime = 0;
-            long currentPosition = 0;
-
-            // Show start position
-            UpdatePositionLabels(0, totalMidiDuration);
-
-            // For each note group
-            for (int i = 0; i < groupedNotes.Count; i++)
-            {
-                // Stop playback if stopped
-                token.ThrowIfCancellationRequested();
-
-                var group = groupedNotes[i];
-                long currentTime = group.Key;
-                var notes = group.ToList();
-
-                // Calculate duration between previous and current times
-                long timeDifference = currentTime - previousTime;
-
-                // If time is elapsed, considered as silence
-                if (timeDifference > 0)
-                {
-                    // Render - wait the silence
-                    currentPosition += timeDifference;
-                    UpdatePositionLabels(currentPosition, totalMidiDuration);
-                    WaitWithCancellation((int)timeDifference, token);
-                }
-
-                // Calculate length of notes
-                int duration = notes.Max(n => n.DeltaTime);
-
-                if (notes.Count == 1)
-                {
-                    // Play single note
-                    var note = notes.First();
-                    int frequency = NoteToFrequency(note.NoteNumber);
-                    NotePlayer.play_note(frequency, duration);
+                    textBox1.Text = fileName;
                 }
                 else
                 {
-                    // Play multiple notes in same time
-                    var frequencies = notes.Select(note => NoteToFrequency(note.NoteNumber)).ToArray();
-                    PlayMultipleNotes(frequencies, duration, token);
-                }
-
-                // Wait length of notes after playing
-                currentPosition += duration;
-                UpdatePositionLabels(currentPosition, totalMidiDuration);
-                WaitWithCancellation(duration, token);
-
-                // Save now for next iteration
-                previousTime = currentTime + duration;
-            }
-
-            // Reset position after playing
-            UpdatePositionLabels(0, totalMidiDuration);
-        }
-
-        // MIDI duration calculation method
-        private static long CalculateTotalMidiDuration(MidiFile midiFile)
-        {
-            long maxTime = 0;
-
-            foreach (var track in midiFile.Events)
-            {
-                foreach (var midiEvent in track)
-                {
-                    // Calculate starting time and duration
-                    long endTime = midiEvent.AbsoluteTime;
-
-                    if (midiEvent is NoteEvent noteEvent)
-                    {
-                        endTime += noteEvent.DeltaTime;
-                    }
-
-                    if (endTime > maxTime)
-                    {
-                        maxTime = endTime;
-                    }
+                    MessageBox.Show("The file you dragged is not supported by NeoBleeper MIDI player or is corrupted.", String.Empty, MessageBoxButtons.OK, MessageBoxIcon.Error);
                 }
             }
-
-            return maxTime;
-        }
-
-        // Public method for stop playback
-        public static void StopPlayback()
-        {
-            if (isPlaying && cancellationTokenSource != null)
-            {
-                cancellationTokenSource.Cancel();
-                isPlaying = false;
-
-                // Reset position when stopped
-                if (staticLabelPosition != null && staticLabelPercentage != null)
-                {
-                    UpdatePositionLabels(0, totalMidiDuration);
-                }
-            }
-        }
-        private static void WaitWithCancellation(int milliseconds, CancellationToken token)
-        {
-            try
-            {
-                Task.Delay(milliseconds, token).Wait(token);
-            }
-            catch (OperationCanceledException)
-            {
-                // Cancelled
-                throw;
-            }
-        }
-
-        
-        private static void PlayMultipleNotes(int[] frequencies, int duration, CancellationToken token)
-        {
-                            Application.DoEvents();
-            int interval = 30; // Switch notes between 30 ms
-            int steps = duration / interval;
-
-            for (int i = 0; i < steps; i++)
-            {
-                // Return if cancelled
-                if (token.IsCancellationRequested)
-                    return;
-
-                foreach (var frequency in frequencies)
-                {
-                    NotePlayer.play_note(frequency, interval);
-                }
-            }
-        }
-
-
-        private static int NoteToFrequency(int noteNumber)
-        {
-            // Frequency from MIDI number
-            return (int)(880.0 * Math.Pow(2.0, (noteNumber - 69) / 12.0));
-        }
-        private void button_play_Click(object sender, EventArgs e)
-        {
-            button_play.Enabled = false;
-            button_stop.Enabled = true;
-            play_MIDI_file(textBox1.Text);
-        }
-        private void button_stop_Click(object sender, EventArgs e)
-        {
-            button_play.Enabled = true;
-            button_stop.Enabled = false;
-            StopPlayback();
-        }
-
-        private void button_rewind_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void MIDI_file_player_FormClosing(object sender, FormClosingEventArgs e)
-        {
-            StopPlayback();
         }
     }
 }
