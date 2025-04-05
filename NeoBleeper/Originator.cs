358e1e4604d25e15b915cec2982178fe9b738fb5
Nisa Özdoğan <nisaozdogan280@gmail.com>
2025-04-05 12:19:21 +0300

    Marking as modified or unmodified issue when note is added or replaced is fixed.

--- NeoBleeper/Originator.cs
+++ NeoBleeper/Originator.cs
@@ -15,8 +15,20 @@ public class Originator
         return new Memento(items);
     }
 
+    public SavedStateMemento CreateSavedStateMemento(int bpmValue, int alternatingNoteLength)
+    {
+        var items = listView.Items.Cast<ListViewItem>().Select(item => (ListViewItem)item.Clone()).ToList();
+        return new SavedStateMemento(items, bpmValue, alternatingNoteLength);
+    }
+
     public void SetMemento(Memento memento)
     {
+        if (memento == null)
+            throw new ArgumentNullException(nameof(memento), "Memento cannot be null");
+
+        if (memento.Items == null)
+            throw new ArgumentException("Memento items cannot be null", nameof(memento));
+
         listView.Items.Clear();
         foreach (var item in memento.Items)
         {
