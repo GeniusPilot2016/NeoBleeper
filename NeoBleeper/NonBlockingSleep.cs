0a3ab193ab96b0869a027270a3092f2f7b663d4f
Nisa Özdoğan <222303026@ogr.uludag.edu.tr>
2025-08-27 22:45:43 +0300

    Accidentally mistyped title in "About NeoBleeper", which is "8" is fixed and CPU usage issue in non-blocking sleep in all computers (except CPU throttled ones) is fixed by optimizing the non-blocking sleep method by using SpinOnce.

--- NeoBleeper/NonBlockingSleep.cs
+++ NeoBleeper/NonBlockingSleep.cs
@@ -9,17 +9,6 @@ namespace NeoBleeper
 {
     public class NonBlockingSleep
     {
-        [DllImport("kernel32.dll")]
-        private static extern uint SetThreadExecutionState(uint esFlags);
-
-        private const uint ES_CONTINUOUS = 0x80000000;
-        private const uint ES_SYSTEM_REQUIRED = 0x00000001;
-        private const uint ES_AWAYMODE_REQUIRED = 0x00000040;
-
-        public static void PreventSleep()
-        {
-            SetThreadExecutionState(ES_CONTINUOUS | ES_SYSTEM_REQUIRED | ES_AWAYMODE_REQUIRED);
-        }
 
         private static readonly long CachedFrequency = Stopwatch.Frequency;
 
@@ -31,38 +20,28 @@ namespace NeoBleeper
             }
 
             long cachedFrequency = CachedFrequency;
-            PreventSleep();
             long currentFrequency = Stopwatch.Frequency;
             long startTicks = Stopwatch.GetTimestamp();
             long targetTicks = startTicks + (milliseconds * currentFrequency) / 1000;
             long coarseTargetTicks = targetTicks - (currentFrequency / 1000000 * 50); // 50 microseconds before target
             int doEventsCounter = 0;
             long now = Stopwatch.GetTimestamp();
-
+            SpinWait spinWait = new SpinWait();
             // Coarse waiting phase
             while (now < coarseTargetTicks)
             {
                 long ticksLeft = coarseTargetTicks - now;
                 double millisecondsLeft = (double)ticksLeft * 1000 / cachedFrequency;
-
-                if (millisecondsLeft > 1)
-                {
-                    // Sleep for a short duration to reduce CPU usage
-                    Thread.Sleep((int)Math.Min(millisecondsLeft / 2, 1));
-                }
-
-                if (Application.MessageLoop && doEventsCounter++ % 10 == 0)
-                {
-                    Application.DoEvents();
-                }
-
+                Application.DoEvents();
+                spinWait.SpinOnce();
                 now = Stopwatch.GetTimestamp();
             }
 
             // Fine-tuning phase
+            
             while (Stopwatch.GetTimestamp() < targetTicks)
             {
-                Thread.Sleep(0);
+                spinWait.SpinOnce();
             }
         }
     }
