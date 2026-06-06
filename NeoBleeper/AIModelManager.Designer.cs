8256cc6a814f05eaf4ecf7502d34930e0a03fe2b
GeniusPilot2016 <nisaozdogan280@gmail.com>
2026-06-07 02:09:27 +0300

    Start to add independent local model browser with character checker
    - Independent local model browser is being added with character checker to make model path selection directly from NeoBleeper that compatible with Ollama application.

--- NeoBleeper/AIModelManager.Designer.cs
+++ NeoBleeper/AIModelManager.Designer.cs
@@ -32,14 +32,19 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AIModelManager));
             flowLayoutPanel1 = new FlowLayoutPanel();
             groupBox1 = new GroupBox();
-            label1 = new Label();
-            textBox1 = new TextBox();
-            checkedListBox1 = new CheckedListBox();
-            label2 = new Label();
-            button1 = new Button();
-            button2 = new Button();
-            button3 = new Button();
+            label4 = new Label();
             icons = new ImageList(components);
+            button5 = new Button();
+            textBox2 = new TextBox();
+            label3 = new Label();
+            button4 = new Button();
+            button3 = new Button();
+            button2 = new Button();
+            button1 = new Button();
+            label2 = new Label();
+            checkedListBox1 = new CheckedListBox();
+            textBox1 = new TextBox();
+            label1 = new Label();
             groupBoxCreateMusicWithAI = new GroupBox();
             buttonResetAPIKey = new Button();
             buttonUpdateAPIKey = new Button();
@@ -48,9 +53,9 @@
             labelAPIKey = new Label();
             textBoxAPIKey = new TextBox();
             labelGoogleGeminiAPIWarning = new Label();
-            button4 = new Button();
             modelsRightClickMenu = new ContextMenuStrip(components);
             removeModelToolStripMenuItem = new ToolStripMenuItem();
+            folderBrowserDialog1 = new FolderBrowserDialog();
             flowLayoutPanel1.SuspendLayout();
             groupBox1.SuspendLayout();
             groupBoxCreateMusicWithAI.SuspendLayout();
@@ -66,11 +71,15 @@
             flowLayoutPanel1.Location = new Point(0, 0);
             flowLayoutPanel1.Name = "flowLayoutPanel1";
             flowLayoutPanel1.Padding = new Padding(5);
-            flowLayoutPanel1.Size = new Size(423, 519);
+            flowLayoutPanel1.Size = new Size(423, 638);
             flowLayoutPanel1.TabIndex = 0;
             // 
             // groupBox1
             // 
+            groupBox1.Controls.Add(label4);
+            groupBox1.Controls.Add(button5);
+            groupBox1.Controls.Add(textBox2);
+            groupBox1.Controls.Add(label3);
             groupBox1.Controls.Add(button4);
             groupBox1.Controls.Add(button3);
             groupBox1.Controls.Add(button2);
@@ -82,88 +91,25 @@
             groupBox1.Font = new Font("HarmonyOS Sans", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
             groupBox1.Location = new Point(8, 8);
             groupBox1.Name = "groupBox1";
-            groupBox1.Size = new Size(407, 262);
+            groupBox1.Size = new Size(407, 379);
             groupBox1.TabIndex = 0;
             groupBox1.TabStop = false;
             groupBox1.Text = "Ollama Settings";
             // 
-            // label1
+            // label4
             // 
-            label1.AutoSize = true;
-            label1.Location = new Point(10, 28);
-            label1.Name = "label1";
-            label1.Size = new Size(108, 16);
-            label1.TabIndex = 0;
-            label1.Text = "Ollama Client URL";
-            // 
-            // textBox1
-            // 
-            textBox1.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
-            textBox1.Location = new Point(10, 47);
-            textBox1.Name = "textBox1";
-            textBox1.Size = new Size(384, 23);
-            textBox1.TabIndex = 1;
-            textBox1.Text = "http://localhost:11434";
-            // 
-            // checkedListBox1
-            // 
-            checkedListBox1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
-            checkedListBox1.FormattingEnabled = true;
-            checkedListBox1.Location = new Point(10, 128);
-            checkedListBox1.Name = "checkedListBox1";
-            checkedListBox1.Size = new Size(384, 94);
-            checkedListBox1.TabIndex = 2;
-            // 
-            // label2
-            // 
-            label2.AutoSize = true;
-            label2.Location = new Point(10, 109);
-            label2.Name = "label2";
-            label2.Size = new Size(88, 16);
-            label2.TabIndex = 3;
-            label2.Text = "Ollama Models";
-            // 
-            // button1
-            // 
-            button1.Anchor = AnchorStyles.Top;
-            button1.ImageIndex = 0;
-            button1.ImageList = icons;
-            button1.Location = new Point(53, 76);
-            button1.Name = "button1";
-            button1.Size = new Size(140, 25);
-            button1.TabIndex = 4;
-            button1.Text = "Update Client URL";
-            button1.TextAlign = ContentAlignment.MiddleRight;
-            button1.TextImageRelation = TextImageRelation.ImageBeforeText;
-            button1.UseVisualStyleBackColor = true;
-            // 
-            // button2
-            // 
-            button2.Anchor = AnchorStyles.Bottom;
-            button2.ImageIndex = 3;
-            button2.ImageList = icons;
-            button2.Location = new Point(210, 228);
-            button2.Name = "button2";
-            button2.Size = new Size(149, 25);
-            button2.TabIndex = 5;
-            button2.Text = "Refresh Models List";
-            button2.TextAlign = ContentAlignment.MiddleRight;
-            button2.TextImageRelation = TextImageRelation.ImageBeforeText;
-            button2.UseVisualStyleBackColor = true;
-            // 
-            // button3
-            // 
-            button3.Anchor = AnchorStyles.Top;
-            button3.ImageIndex = 1;
-            button3.ImageList = icons;
-            button3.Location = new Point(199, 76);
-            button3.Name = "button3";
-            button3.Size = new Size(140, 25);
-            button3.TabIndex = 6;
-            button3.Text = "Reset Client URL";
-            button3.TextAlign = ContentAlignment.MiddleRight;
-            button3.TextImageRelation = TextImageRelation.ImageBeforeText;
-            button3.UseVisualStyleBackColor = true;
+            label4.Anchor = AnchorStyles.Top;
+            label4.ImageAlign = ContentAlignment.TopLeft;
+            label4.ImageIndex = 6;
+            label4.ImageList = icons;
+            label4.ImeMode = ImeMode.NoControl;
+            label4.Location = new Point(5, 165);
+            label4.Margin = new Padding(2, 0, 2, 0);
+            label4.Name = "label4";
+            label4.Size = new Size(397, 38);
+            label4.TabIndex = 11;
+            label4.Text = "     Warning: Do not select folders containing accented, non-English, or special characters, otherwise, it may cause errors.";
+            label4.TextAlign = ContentAlignment.TopCenter;
             // 
             // icons
             // 
@@ -178,6 +124,134 @@
             icons.Images.SetKeyName(5, "icons8-mark-view-as-hidden-48.png");
             icons.Images.SetKeyName(6, "icons8-warning-48.png");
             icons.Images.SetKeyName(7, "icons8-update-48.png");
+            icons.Images.SetKeyName(8, "icons8-browse-folder-48.png");
+            // 
+            // button5
+            // 
+            button5.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            button5.ImageIndex = 8;
+            button5.ImageList = icons;
+            button5.Location = new Point(289, 130);
+            button5.Name = "button5";
+            button5.Size = new Size(105, 25);
+            button5.TabIndex = 10;
+            button5.Text = "Browse";
+            button5.TextAlign = ContentAlignment.MiddleRight;
+            button5.TextImageRelation = TextImageRelation.ImageBeforeText;
+            button5.UseVisualStyleBackColor = true;
+            button5.Click += button5_Click;
+            // 
+            // textBox2
+            // 
+            textBox2.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            textBox2.Location = new Point(10, 132);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(273, 23);
+            textBox2.TabIndex = 9;
+            textBox2.Text = "C:\\Users\\%USERPROFILE%\\.ollama";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(10, 113);
+            label3.Name = "label3";
+            label3.Size = new Size(111, 16);
+            label3.TabIndex = 8;
+            label3.Text = "Ollama Model Path";
+            // 
+            // button4
+            // 
+            button4.Anchor = AnchorStyles.Bottom;
+            button4.ImageIndex = 2;
+            button4.ImageList = icons;
+            button4.Location = new Point(42, 345);
+            button4.Name = "button4";
+            button4.Size = new Size(162, 25);
+            button4.TabIndex = 7;
+            button4.Text = "Download New Model";
+            button4.TextAlign = ContentAlignment.MiddleRight;
+            button4.TextImageRelation = TextImageRelation.ImageBeforeText;
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Anchor = AnchorStyles.Top;
+            button3.ImageIndex = 1;
+            button3.ImageList = icons;
+            button3.Location = new Point(199, 76);
+            button3.Name = "button3";
+            button3.Size = new Size(140, 25);
+            button3.TabIndex = 6;
+            button3.Text = "Reset Client URL";
+            button3.TextAlign = ContentAlignment.MiddleRight;
+            button3.TextImageRelation = TextImageRelation.ImageBeforeText;
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // button2
+            // 
+            button2.Anchor = AnchorStyles.Bottom;
+            button2.ImageIndex = 3;
+            button2.ImageList = icons;
+            button2.Location = new Point(210, 345);
+            button2.Name = "button2";
+            button2.Size = new Size(149, 25);
+            button2.TabIndex = 5;
+            button2.Text = "Refresh Models List";
+            button2.TextAlign = ContentAlignment.MiddleRight;
+            button2.TextImageRelation = TextImageRelation.ImageBeforeText;
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            button1.Anchor = AnchorStyles.Top;
+            button1.ImageIndex = 0;
+            button1.ImageList = icons;
+            button1.Location = new Point(53, 76);
+            button1.Name = "button1";
+            button1.Size = new Size(140, 25);
+            button1.TabIndex = 4;
+            button1.Text = "Update Client URL";
+            button1.TextAlign = ContentAlignment.MiddleRight;
+            button1.TextImageRelation = TextImageRelation.ImageBeforeText;
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(10, 207);
+            label2.Name = "label2";
+            label2.Size = new Size(88, 16);
+            label2.TabIndex = 3;
+            label2.Text = "Ollama Models";
+            // 
+            // checkedListBox1
+            // 
+            checkedListBox1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            checkedListBox1.FormattingEnabled = true;
+            checkedListBox1.Location = new Point(10, 227);
+            checkedListBox1.Name = "checkedListBox1";
+            checkedListBox1.Size = new Size(384, 112);
+            checkedListBox1.TabIndex = 2;
+            // 
+            // textBox1
+            // 
+            textBox1.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            textBox1.Location = new Point(10, 47);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(384, 23);
+            textBox1.TabIndex = 1;
+            textBox1.Text = "http://localhost:11434";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(10, 28);
+            label1.Name = "label1";
+            label1.Size = new Size(108, 16);
+            label1.TabIndex = 0;
+            label1.Text = "Ollama Client URL";
             // 
             // groupBoxCreateMusicWithAI
             // 
@@ -189,7 +263,7 @@
             groupBoxCreateMusicWithAI.Controls.Add(labelAPIKey);
             groupBoxCreateMusicWithAI.Controls.Add(textBoxAPIKey);
             groupBoxCreateMusicWithAI.Font = new Font("HarmonyOS Sans", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            groupBoxCreateMusicWithAI.Location = new Point(8, 276);
+            groupBoxCreateMusicWithAI.Location = new Point(8, 393);
             groupBoxCreateMusicWithAI.Name = "groupBoxCreateMusicWithAI";
             groupBoxCreateMusicWithAI.Size = new Size(407, 179);
             groupBoxCreateMusicWithAI.TabIndex = 12;
@@ -290,27 +364,13 @@
             labelGoogleGeminiAPIWarning.ImageIndex = 6;
             labelGoogleGeminiAPIWarning.ImageList = icons;
             labelGoogleGeminiAPIWarning.ImeMode = ImeMode.NoControl;
-            labelGoogleGeminiAPIWarning.Location = new Point(8, 458);
+            labelGoogleGeminiAPIWarning.Location = new Point(8, 575);
             labelGoogleGeminiAPIWarning.Name = "labelGoogleGeminiAPIWarning";
             labelGoogleGeminiAPIWarning.Size = new Size(407, 51);
             labelGoogleGeminiAPIWarning.TabIndex = 14;
             labelGoogleGeminiAPIWarning.Text = "    In your region (European Economic Area, Switzerland, or United Kingdom), the Google Gemini™ API may only be available as a \r\npaid service. Please ensure your API key is for a paid account.";
             labelGoogleGeminiAPIWarning.TextAlign = ContentAlignment.TopCenter;
             labelGoogleGeminiAPIWarning.Visible = false;
-            // 
-            // button4
-            // 
-            button4.Anchor = AnchorStyles.Bottom;
-            button4.ImageIndex = 2;
-            button4.ImageList = icons;
-            button4.Location = new Point(42, 228);
-            button4.Name = "button4";
-            button4.Size = new Size(162, 25);
-            button4.TabIndex = 7;
-            button4.Text = "Download New Model";
-            button4.TextAlign = ContentAlignment.MiddleRight;
-            button4.TextImageRelation = TextImageRelation.ImageBeforeText;
-            button4.UseVisualStyleBackColor = true;
             // 
             // modelsRightClickMenu
             // 
@@ -331,7 +391,7 @@
             AutoScaleDimensions = new SizeF(96F, 96F);
             AutoScaleMode = AutoScaleMode.Dpi;
             AutoSize = true;
-            ClientSize = new Size(423, 519);
+            ClientSize = new Size(423, 638);
             Controls.Add(flowLayoutPanel1);
             FormBorderStyle = FormBorderStyle.FixedSingle;
             MaximizeBox = false;
@@ -371,5 +431,10 @@
         private Button button4;
         private ContextMenuStrip modelsRightClickMenu;
         private ToolStripMenuItem removeModelToolStripMenuItem;
+        private TextBox textBox2;
+        private Label label3;
+        private Button button5;
+        private FolderBrowserDialog folderBrowserDialog1;
+        private Label label4;
     }
 }
--- NeoBleeper/AIModelManager.cs
+++ NeoBleeper/AIModelManager.cs
@@ -17,7 +17,7 @@ namespace NeoBleeper
             InitializeComponent();
             bool isOllamaInstalled = IsOllamaInstalled();
             bool isGoogleGeminiAvailable = CreateMusicWithAI.IsAvailableInCountry();
-            if(!isOllamaInstalled && !isGoogleGeminiAvailable)
+            if (!isOllamaInstalled && !isGoogleGeminiAvailable)
             {
                 MessageForm.Show("Ollama is not installed on this system, and it appears that Google Gemini™ is not available in your country. Please install Ollama to use the AI Model Manager.", "Ollama Not Found", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 this.Close();
@@ -29,6 +29,10 @@ namespace NeoBleeper
                     int heightWillBeSubtracted = groupBox1.Height + flowLayoutPanel1.Padding.All;
                     flowLayoutPanel1.Controls.Remove(groupBox1);
                     this.Height -= heightWillBeSubtracted;
+                }
+                else
+                {
+                    textBox1.Text = Settings1.Default.OllamaClientURL; // Pre-fill the text box with the saved Ollama Client URL from settings
                 }
                 if (!isGoogleGeminiAvailable)
                 {
@@ -96,6 +100,101 @@ namespace NeoBleeper
             {
                 return false;
             }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(textBox1.Text))
+            {
+                MessageForm.Show("Please enter the URL for the Ollama Client.", "URL Required", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            if (!textBox1.Text.StartsWith("http://") && !textBox1.Text.StartsWith("https://"))
+            {
+                MessageForm.Show("Please enter a valid URL that starts with http:// or https://", "Invalid URL", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            Settings1.Default.OllamaClientURL = textBox1.Text.Trim(); // Save the URL to settings
+            Settings1.Default.Save(); // Persist the settings
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Settings1.Default.OllamaClientURL = "http://localhost:11434"; // Reset to default URL
+            Settings1.Default.Save(); // Persist the settings 
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            string folder = folderBrowserDialog1.ShowDialog() == DialogResult.OK ? folderBrowserDialog1.SelectedPath : null;
+            if (folder != null)
+            {
+                if (!string.IsNullOrWhiteSpace(folder))
+                {
+                    if (CheckForStrictlyForbiddenCharacters(folder))
+                    {
+                        MessageForm.Show("The selected folder path contains characters that are strictly forbidden in file paths (such as <, >, :, \", |, ?, *). Please choose a different folder without these characters.", "Invalid Folder Path", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                        return;
+                    }
+                    if (DoesFolderNameContainSpecialCharacters(folder))
+                    {
+                        DialogResult result = MessageForm.Show("The selected folder path contains special characters (such as emojis, non-Latin characters, or other symbols) that may cause issues with file system operations or with Ollama. Do you want to proceed with this folder?", "Special Characters Detected", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+                        if (result == DialogResult.Yes)
+                        {
+                            textBox2.Text = folder; // Set the selected folder path to the text box
+                        }
+                    }
+                    else
+                    {
+                        textBox2.Text = folder; // Set the selected folder path to the text box
+                    }
+                }
+            }
+        }
+        /// <summary>
+        /// Checks if the provided folder path contains any special characters that are not allowed in file paths
+        /// such as emojis, non-Latin characters, or other symbols that could cause issues with file system operations. 
+        /// This method iterates through the set of invalid path characters defined by the .NET framework and checks if 
+        /// any of them are present in the folder path. Also, if these paths are used, the Ollama may give an error like 
+        /// "Error: 500 Internal Server Error: llama-server process has terminated: exit status 1: error loading model: 
+        /// llama_model_loader: failed to load model from {path}" error that can be caused by special characters in the folder path. 
+        /// If any special character is found, the method returns true, indicating that the folder path may not be suitable for use in file system operations or with Ollama.
+        /// 
+        /// </summary>
+        /// <param name="folderPath"></param>
+        /// <returns> true if the folder path contains special characters; otherwise, false.</returns>
+        private bool DoesFolderNameContainSpecialCharacters(string folderPath)
+        {
+            if (string.IsNullOrWhiteSpace(folderPath))
+                return true;
+
+            foreach (char c in folderPath)
+            {
+                if (char.IsControl(c))
+                    return true;
+
+                if (c > 127) // ç, ğ, ü, ş, ö, emojis vb.
+                    return true;
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// Checks if the provided folder path contains any characters that are strictly forbidden in file paths, such as <, >, :, ", |, ?, *, and others defined by the .NET framework. This method uses the Path.GetInvalidPathChars() method to retrieve the set of invalid characters and checks if any of them are present in the folder path. If any forbidden character is found, the method returns true, indicating that the folder path is not valid for use in file system operations.
+        /// </summary>
+        /// <param name="folderPath"></param>
+        /// <returns>true if the folder path contains strictly forbidden characters; otherwise, false.</returns>
+
+        private bool CheckForStrictlyForbiddenCharacters(string folderPath)
+        {
+            char[] invalidChars = System.IO.Path.GetInvalidPathChars();
+            foreach (char c in invalidChars)
+            {
+                if (folderPath.Contains(c))
+                    return true;
+            }
+            return false;
         }
     }
 }
