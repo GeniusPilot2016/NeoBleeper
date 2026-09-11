b1dff8999b15fd9b9a3e6bf5296d28090a41c269
GeniusPilot2016 <nisaozdogan280@gmail.com>
2026-09-11 10:55:28 +0300

    Remove RegEx of legacy Gemini API key
    - RegEx of legacy Gemini API key is removed to comply new API key format of Gemini API that Gemini API Auth keys are introduced and legacy Gemini API key will be deprecated in this month.

--- NeoBleeper/CreateMusicWithAI.cs
+++ NeoBleeper/CreateMusicWithAI.cs
@@ -630,17 +630,13 @@ namespace NeoBleeper
         /// <returns>true if the API key matches the expected Google API key format; otherwise, false.</returns>
         public static bool IsAPIKeyValidFormat(string APIKey)
         {
-            // Google API keys typically start with "AIzaSy" followed by 33 alphanumeric characters, underscores, or hyphens
-            // or start with "AQ.Ab8RN6" followed by 47 alphanumeric characters, underscores, or hyphens
+            // Google API keys typically start with "AQ.Ab8RN6" followed by 47 alphanumeric characters, underscores, or hyphens
             if (string.IsNullOrWhiteSpace(APIKey))
                 return false;
 
-            // Regex pattern to match the legacy Gemini API key format
-            var regex1 = new Regex(@"AIzaSy[A-Za-z0-9_\-]{33}$");
-
-            // Regex pattern to match the new Google API key format
-            var regex2 = new Regex(@"^AQ\.Ab8RN6[A-Za-z0-9_-]{44}$");
-            return regex1.IsMatch(APIKey) || regex2.IsMatch(APIKey);
+            // Regex pattern to match the Gemini API Auth Key format: "AQ.Ab8RN6" followed by 44 alphanumeric characters, underscores, or hyphens
+            var regex = new Regex(@"^AQ\.Ab8RN6[A-Za-z0-9_-]{44}$");
+            return regex.IsMatch(APIKey);
         }
 
         /// <summary>
